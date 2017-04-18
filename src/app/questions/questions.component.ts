bddfb04a79f7036c1ad92553de8bb83aae138125
Amar <sharewithamar@gmail.com>
2017-04-18 23:57:20 +0530

    rank js logic

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -54,6 +54,14 @@ this.questions=[
 
   }
 
+ addRank(){
+   // Rank Logic
+
+  /* var arr = [79, 5, 18, 5, 32, 1, 16, 1, 82, 13];
+var sorted = arr.slice().sort(function(a,b){return b-a})
+var ranks = arr.slice().map(function(v){ return sorted.indexOf(v)+1 });*/
+ }
+
   caclulateScore()
   {
     let answers =["Delhi","Chennai"];
